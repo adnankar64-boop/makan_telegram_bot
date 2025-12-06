61af083613d0ded96024e160b03e1ff099770807
adnankar64-boop <adnankar64@gmail.com>
2025-12-06 20:07:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import sys import os import json import logging import time import threading import requests # اضافه کردن مسیر ریشه sys.path.append(os.path.dirname(__file__)) from hyperdash_telegram_bot_mtproto_coinglass import main as bot_main if __name__ == "__main__": bot_main()
+import sys
+import os
+import json
+import logging
+import time
+import threading
+import requests
+
+# اضافه کردن مسیر ریشه
+sys.path.append(os.path.dirname(__file__))
+
+from hyperdash_telegram_bot_mtproto_coinglass import main as bot_main
+
+if __name__ == "__main__":
+    bot_main()

6b8d615c5d180aceed4ad710928c40b7273ef5a6
adnankar64-boop <adnankar64@gmail.com>
2025-12-11 23:03:21 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 
 sys.path.append(os.path.dirname(__file__))
 
-import asyncio
 from hyperdash_telegram_bot_mtproto_coinglass import main as bot_main
 
-asyncio.run(bot_main())
-
+# هیچ asyncio لازم نیست
+bot_main()
